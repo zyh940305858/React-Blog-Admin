0ac48dd73c96201d8f39642aa9b5c4ff682dc425
zyh940305858 <940305858@qq.com>
2020-04-14 23:47:26 +0800

    Finish Layout

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 ## 项目模块
 - [x] 项目初始化 (Project init)
 - [x] 路由搭建 (Router construct)
+- [x] 完成Layout (Finish Layout)
 - [x] 页面模块 (Pages module)
     - [x] 登陆页面 (Login page)
---
+++ src/Pages/AddArticle.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import marked from 'marked';
+import '../static/css/AddArticle.css';
+import { Row, Col, Input, Select, Button, DatePicker } from 'antd';
+
+const { Option } = Select;
+const { TextArea } = Input;
+
+function AddArticle() {
+    return (
+        <div>我是添加文章页面</div>
+    )
+};
+
+export default AddArticle;
